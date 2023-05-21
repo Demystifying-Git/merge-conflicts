a49d7b019c85bcd91818331164820b62888cce26
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-05-21 12:05:36 +0200

    add multiplication

--- Calculator.swift
+++ Calculator.swift
@@ -1,7 +1,15 @@
 class Calculator {
 
+    func sum(a: Float, b: Float) -> Float {
+        return a + b
+    }
+
     func difference(a: Int, b: Int) -> Int {
         return a - b
+    }
+
+    func multiplication(a: Int, b: Int) -> Int {
+        return a * b
     }
 
 }
